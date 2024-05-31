0ae8e59ddc4b8aad21778a4ff59c66082f7dadc9
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-05-31 12:25:36 -0500

    c sharp stuff

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.WriteLine("Hello, World!!!");
+
+// int blarghie = 15;
+// string name = "Bob ross";
+// var boom = " " + 15.678;
+// Console.WriteLine(blarghie + " " + name + boom);
+
+//Array crud
+//create
+string[] favoriteRats = ["fancy rat", "brown rat", "radioactive rat"];
+
+//read
+foreach(var rat in favoriteRats)
+{
+  Console.WriteLine(rat);
+}
+
+//update
+// this works but use linq favoriteRats[0] = "Fancy Rat";
+var newFavoriteRats = favoriteRats.Where((e)=> e.StartsWith("b"));
+
+foreach(var rat in newFavoriteRats)
+{
+  Console.WriteLine(rat);
+}
+
+//delete
+//gimme can't really delete from an array well. 
